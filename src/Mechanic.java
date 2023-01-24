7ba877f6320c4c3a50d467e001d0b9980eb31554
Don1ns <riaz.karimullin@yandex.ru>
2023-01-24 11:22:56 +0300

    Коллекции Часть 2. Исправленный

--- src/Mechanic.java
+++ src/Mechanic.java
@@ -1,5 +1,7 @@
 import transport.Transport;
 
+import java.util.Objects;
+
 public class Mechanic <T extends Transport<?>> {
     private String fullName;
     private String company;
@@ -34,5 +36,18 @@ public class Mechanic <T extends Transport<?>> {
     @Override
     public String toString() {
         return "Механик " + fullName + ". Из компании " + company;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mechanic<?> mechanic = (Mechanic<?>) o;
+        return Objects.equals(fullName, mechanic.fullName) && Objects.equals(company, mechanic.company);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, company);
     }
 }
